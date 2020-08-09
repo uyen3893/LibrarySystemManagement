d6604efb1d962bcd22bc8194b9aaf015902eae89
uyen3893 <uyen3893@gmail.com>
2020-08-09 15:43:34 +0700

    Update int class

--- src/ConsoleClient/Helpers/IO_Boolean.java
+++ src/ConsoleClient/Helpers/IO_Boolean.java
@@ -2,6 +2,10 @@ package ConsoleClient.Helpers;
 
 public class IO_Boolean {
 	public static boolean inputBoolean(String note, String trueValue, String falseValue) {
-		
+		boolean condition = false;
+		boolean result = true;
+		do {
+			
+		} while(true);
 	}
 }
--- src/ConsoleClient/Helpers/IO_INT.java
+++ src/ConsoleClient/Helpers/IO_INT.java
@@ -3,15 +3,45 @@ package ConsoleClient.Helpers;
 import java.util.Scanner;
 
 public class IO_INT {
-	public static int inputInt(String Note, int min = 0, int max = 20) {
+	
+	public static int inputInt(String note, int min, int max) {
+		
+		
 		boolean condition = false;
-		int result;
+		int result = 0;
 		do {
-			IO_String.outputString(Note);
+			IO_String.outputString(note);
 			Scanner scanner = new Scanner(System.in);
-			String input = scanner.nextLine();
+			int input = Integer.parseInt(scanner.nextLine());
+			scanner.close();
+			if (input >= min && input <= max) {
+				condition = true;
+			} else condition = false;
+			if(condition) {
+				result = input;
+				break;
+			}
 			
+		} while(!condition);
+		return result;
+		
+	}
+	
+	public static int inputInt(String note) {
+		boolean condition = false;
+		int result = 0;
+		do {
+			IO_String.outputString(note);
+			Scanner scanner = new Scanner(System.in);
+			int input = Integer.parseInt(scanner.nextLine());
+			scanner.close();
 			
-		}
+			if(condition) {
+				result = input;
+				break;
+			}
+			
+		} while(!condition);
+		return result;
 	}
 }
